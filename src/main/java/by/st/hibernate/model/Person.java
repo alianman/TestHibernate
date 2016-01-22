a9798e6dffa774cce28923eeefc9668b32827d4b
alianman <alian.object@gmail.com>
2016-01-22 09:58:08 +0200

    Initial commit

---
+++ src/main/java/by/st/hibernate/model/Person.java
@@ -0,0 +1,71 @@
+package by.st.hibernate.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by Administrator on 20.01.16.
+ */
+public class Person implements Serializable {
+    private long id;
+    private String name;
+    private String address;
+
+    public Person(int id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    public Person(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public Person() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (getId() != person.getId()) return false;
+        if (getName() != null ? !getName().equals(person.getName()) : person.getName() != null) return false;
+        return !(getAddress() != null ? !getAddress().equals(person.getAddress()) : person.getAddress() != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (getId() ^ (getId() >>> 32));
+        result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+        result = 31 * result + (getAddress() != null ? getAddress().hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/test/java/by/st/hibernate/PersonTest.java
@@ -0,0 +1,103 @@
+package by.st.hibernate;
+
+import by.st.hibernate.model.Person;
+import by.st.hibernate.utils.HibernateUtils;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 22.01.16.
+ */
+public class PersonTest extends Assert {
+
+    private static Session session;
+
+    @Before
+    public void openSession() {
+        try {
+            session = HibernateUtils.getSession();
+        } catch (Throwable e) {
+            fail("failed to open session");
+        }
+    }
+
+    @Test
+    public void testGetPerson() {
+        Person testPerson = new Person("Alexei Petkevich", "Minsk");
+        session.beginTransaction();
+        session.save(testPerson);
+        Person extractedPerson = (Person) session.get(Person.class, testPerson.getId());
+        session.delete(testPerson);
+        session.getTransaction().commit();
+        String errorMessage = "testGetPerson failed: saved and extracted objects are not equal";
+        if (extractedPerson == null) {
+            fail(errorMessage);
+        }
+        assertEquals(errorMessage, testPerson, extractedPerson);
+        assertEquals(errorMessage, "Alexei Petkevich", testPerson.getName());
+        assertEquals(errorMessage, "Minsk", testPerson.getAddress());
+        assertEquals(errorMessage, "Alexei Petkevich", extractedPerson.getName());
+        assertEquals(errorMessage, "Minsk", extractedPerson.getAddress());
+    }
+
+    @Test
+    public void testGetPerson_ChangedName() {
+        Person testPerson = new Person("Alexei Petkevich", "Minsk");
+        session.beginTransaction();
+        session.save(testPerson);
+        testPerson.setName("test");
+        Person extractedPerson = (Person) session.get(Person.class, testPerson.getId());
+        session.delete(testPerson);
+        session.getTransaction().commit();
+        String errorMessage = "testGetPerson_ChangedName failed: saved and extracted objects are not equal";
+        if (extractedPerson == null) {
+            fail(errorMessage);
+        }
+        assertEquals(errorMessage, testPerson, extractedPerson);
+        assertEquals(errorMessage, "test", extractedPerson.getName());
+    }
+
+    @Test
+    public void testLoadPerson() {
+        Person testPerson = new Person("Alexei Petkevich", "Minsk");
+        Person extractedPerson = null;
+        String errorMessage = "testLoadPerson failed: saved and extracted objects are not equal";
+        try {
+            session.beginTransaction();
+            session.save(testPerson);
+            extractedPerson = (Person) session.load(Person.class, testPerson.getId());
+            session.delete(testPerson);
+            session.getTransaction().commit();
+
+        } catch (HibernateException e) {
+            errorMessage = "testLoadPerson failed: " + e.getMessage();
+            fail(errorMessage);
+        }
+
+        assertEquals(errorMessage, testPerson, extractedPerson);
+        assertEquals(errorMessage, "Alexei Petkevich", testPerson.getName());
+        assertEquals(errorMessage, "Minsk", testPerson.getAddress());
+        assertEquals(errorMessage, "Alexei Petkevich", extractedPerson.getName());
+        assertEquals(errorMessage, "Minsk", extractedPerson.getAddress());
+    }
+
+    @Test
+    public void testSaveOrUpdatePerson() {
+        Person testPerson = new Person("Alexei Petkevich", "Minsk");
+        session.beginTransaction();
+        session.save(testPerson);
+        testPerson.setAddress("testAddress");
+        session.getTransaction().commit();
+        String errorMessage = "testSaveOrUpdatePerson failed: address error";
+        assertEquals(errorMessage, "testAddress", testPerson.getAddress());
+    }
+
+    @After
+    public void closeSession() {
+        session.close();
+    }
+}
